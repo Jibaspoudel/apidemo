77b5aaeccfca99d7a8cf69a4f228a1f03750ed75
Jibaspoudel <94658376+Jibaspoudel@users.noreply.github.com>
2026-01-26 23:48:28 -0500

    Delete src directory

--- src/main/java/com/restapi/rest/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restapi.rest.repository;
-
-import com.restapi.rest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmailIgnoreCase(String email);
-}
