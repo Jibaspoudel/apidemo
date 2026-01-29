78d5fc56129cbb1fa8b1ce2e6b3e16eb493347b9
jibaspoudel <jibaspoudel143@gmail.com>
2026-01-28 21:59:38 -0500

    edit

--- src/test/java/com/restapi/rest/RestApplicationTests.java
+++ src/test/java/com/restapi/rest/RestApplicationTests.java
@@ -9,4 +9,19 @@ class RestApplicationTests {
     @Test
     void contextLoads() {
     }
+
+//    @Test
+//    void getUsernameByIdreturnValid(){
+//        //MOCK RUBYOBJ if jparepo.getUsernameById(1)
+//        //String name = getUsernameById(1);
+//        //asserttrue("ruby",name);
+//    }
+//
+//    @Test
+//    void getUsernameByIdreturnNull(){
+//        //String name = getUsernameById(1);
+//        //asserttrue(null,name);
+//    }
+
+
 }
