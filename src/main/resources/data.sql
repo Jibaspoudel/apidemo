31f9e40fc22bf3ff547fe3d9532334762e0524ec
jibaspoudel <jibaspoudel143@gmail.com>
2026-01-28 21:59:54 -0500

    edit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, email) VALUES ("Ruby", "rubysinghthakuri@gmail.com");
+INSERT INTO users(name, email) VALUES ("Jibas", "Jibaspoudel@gmail.com");
+INSERT INTO users(name,email) VALUES ("shangita", "Shangita@yahoo.com");
